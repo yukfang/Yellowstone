298b49e1be13f1639862a5d0f9c00d7ee5a93276
yfa <yfa>
2023-07-23 13:47:49 +0800

    test

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const {koaApp, init} = require('./koaApp');
 const PORT = process.env.PORT || 80
+// import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require('./openai');
+const proxying = require('./utils/http/proxying');
 
 if(process.env.PLATFORM == 'FAAS') { // Set this ENV in FAAS to make it work
     exports.handler = koaApp.callback();
@@ -7,4 +10,35 @@ if(process.env.PLATFORM == 'FAAS') { // Set this ENV in FAAS to make it work
 } else { // Local Test
     init();
     koaApp.listen(PORT);
+}
+
+
+
+async function main() {
+    const configuration = new Configuration({
+        organization: "org-cMqDvEaOJiyWZ0zfxqURSW2L",
+        apiKey: process.env.OPENAI_API_KEY || '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',
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.listEngines();
+    
+    console.log(response)
+}
+
+
+async function listModels() {
+    const endpoint      = 'https://api.openai.com/v1/models';
+    const method        = 'GET';
+
+    let header      = {Cookie: cookie.value}
+    let param       = {
+        order_id,
+        archive_after: true,
+        lang: 'EN'
+    };
+    let body        = null;
+
+
+    const response = (await proxying(method, endpoint, header, param, body, true));
+    // console.log(response.data)
 }
