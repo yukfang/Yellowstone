8026f4189a7989b6670d65f9ad5ea8c3dc50237c
yfa <yfa>
2023-07-28 16:26:03 +0800

    shorten status

--- koaApp.js
+++ koaApp.js
@@ -221,6 +221,11 @@ async function buildBody(detail, tags){
             // console.log(tags)
             main_status   = tags[0]?.name || ''
             sub_status    = tags[0].sub_tags[0]?.name || main_status
+            if(sub_status.includes("Pending with GBS")) {
+                sub_status = "Pending GBS Comms"
+            } else if(sub_status.includes("Pending with Client")) {
+                sub_status = "Pending Client Assessment"
+            }
             // console.log(`${main_status} ${sub_status} lololo`)
         }
     } catch(err) {
