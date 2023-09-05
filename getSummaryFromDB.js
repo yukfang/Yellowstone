9cb534aef7884f281fa0f5c15275b0d16e70418a
Leona Fang <fangyu.k@bytedance.com>
2023-09-05 18:28:00 +0800

    fix tag

--- getSummaryFromDB.js
+++ getSummaryFromDB.js
@@ -5,12 +5,6 @@ const TABLES    =   require('./database/table')
 const cachePath = `./LocalCache`
 
 async function fetchSummaryDb(order_id){
-    /** Find tag in LocalCache */
-    const tagFilePath = `${cachePath}/${order_id}.json`
-    if(!fs.existsSync(tagFilePath)) {
-        await fs.writeFileSync(tagFilePath, JSON.stringify({order_id, refresh:"2020-01-01T01:01:01Z", tag:[]}))
-    }
-
     /** Find record in DB */
     const OrderInfoTable = await TABLES.OrderInfo2;
     const order = await OrderInfoTable.findOne({
@@ -32,24 +26,10 @@ async function fetchSummaryDb(order_id){
         return summary
     }
 
-    /** Construct summary by DB rec and local tag */
-    console.log(`Order found in db: ${order.dataValues.order_id}`)
-    if(fs.existsSync(tagFilePath)) {
-        const tagdata = await ((f)=>{
-            return new Promise((resolve) => {
-                resolve(JSON.parse(fs.readFileSync(f, {encoding:'utf8', flag:'r'})))
-            }).catch(err=>{
-                console.log('JSON Parse error ' + err)
-            })
-        })(tagFilePath)
+    const status = await extractTags(order.tag)
+    order.summary.main_status = status.main_status
+    order.summary.sub_status  = status.sub_status
 
-        // console.log(`Tag data ${JSON.stringify(tagdata)}`)
-        const tag = await extractTags(tagdata.tag)
-
-        // Use Local Cache to overwrite DB
-        // order.summary.main_status = tag.main_status
-        // order.summary.sub_status  = tag.sub_status
-    }
     return order.summary
 }
 
--- syncTags.js
+++ syncTags.js
@@ -1,52 +1,37 @@
+const TABLES =  require('./database/table')
+const token  = require('./utils/athena/cookie')
 const getOrderTag       = require('./utils/athena/tag')
-const fs                = require('fs')
-const token             = require('./utils/athena/cookie')
-const delayms           = (ms) => new Promise((res, rej) => {setTimeout(res, ms * 1)})
 
- 
+const extractTags = require('./utils/report/status')
+const delayms = (ms) => new Promise((res, rej) => {setTimeout(res, ms * 1)})
+
 async function tagTask() {
     console.log(`>>>>>> Start Timer Refreshing...`)
     await token();
-    const cachePath = `./LocalCache`
-    if (!fs.existsSync(cachePath)){
-        fs.mkdirSync(cachePath);
-        return;
-    }
 
-    const files = fs.readdirSync(cachePath)
-    let tags = []
-    for(let i = 0; i < files.length; i++) {
-        const filePath = `${cachePath}/${files[i]}`
-        if(fs.existsSync(filePath)) {
-            const tag = await ((f)=>{
-                return new Promise((resolve) => {
-                    resolve(JSON.parse(fs.readFileSync(f, {encoding:'utf8', flag:'r'})))
-                }).catch(err=>{
-                    console.log('JSON Parse error ' + err)
-                })
-            })(filePath)
+    const OrderTable = await TABLES.OrderInfo2
+    let orders = (await OrderTable.findAll({
+        attributes: ['order_id', 'updatedAt']
+    }))?.map(o => o.dataValues)
+ 
+    orders = orders.sort((a,b)=> a.updatedAt - b.updatedAt).slice(0,5)
 
-            tags.push(tag)
-        }
-    }
-    tags = tags.sort((a,b)=>a.refresh - b.refresh).slice(0,5)
+    console.table(orders)
 
-    console.table(tags)
+    for(let i = 0; i < orders.length; i++) {
+        const order_id =  (orders[i]).order_id
+        const rawtag = await getOrderTag(order_id)
+        const status = extractTags(rawtag)
 
-    for(let i = 0; i < tags.length; i++) {
-        const order_id =  (tags[i]).order_id
-        const refresh   = (new Date(tags[i].refresh || '2020-01-01T01:01:01Z')).getTime()
-        const now       = Date.now()
 
-         if(now - refresh > 1000 * 30) {
-            console.log(`---------Start refresh for ${order_id}---------`)
-            const tag = await getOrderTag(order_id)
-            fs.writeFileSync(`${cachePath}/${order_id}.json`, JSON.stringify({
-                order_id,  refresh : new Date(now), tag
-            }, null , 2 ))
-        }
-
-        await delayms(256)
+        await OrderTable.update({
+            order_id    : order_id,
+            tag         : rawtag
+        }, {
+            where: {
+                order_id
+            },
+        })
     }
     console.log(`... End Tag Refreshing <<<`)
 }
