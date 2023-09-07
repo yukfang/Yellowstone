cc208b958656589ade2459975cc3b04dbd37629b
Yuk Fang <yfa@YukdeMacBook-Pro.local>
2023-09-07 22:22:52 +0800

    add platform

---
+++ utils/report/platform.js
@@ -0,0 +1,31 @@
+function extract(detail){
+    const replies = detail.replies
+    const reg =     /(.*)(\[method=(.*?)\])(.*)/i
+
+    let platform = ''
+
+    const reg_exp = reg
+    if(replies) {
+        for(let k = 0; k < replies.length; k++) {
+            const reply = replies[k];
+            const items = reply.items.filter(x => x.type == 6);
+
+            for(let j = 0; j < items.length; j++) {
+                const item = items[j];
+                const matches = item.content.match(reg_exp)
+                if(matches) {
+                    platform = matches[3]
+                }
+            }
+        }
+    }
+    return platform.toLowerCase()
+}
+
+function test() {
+ 
+}
+
+// test();
+
+module.exports = extract
